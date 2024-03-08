2376b10aee7184600afce9088eb2b9b7c2ce44be
Rohan Neure <114942902+neurerohan@users.noreply.github.com>
2024-03-08 17:43:07 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    // You can add your form submission logic here
+    alert("Form submitted!"); // Example alert
+});
